15ed83d9d7c58c47821e1cb3ed5be30a61109801
jshhrrsn <joshharrison@atreides.ai>
2021-07-19 11:53:56 +0100

    start async refactoring

--- Repositories/IItemsRepository.cs
+++ Repositories/IItemsRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Catalog.Entities;
 
 namespace Catalog.Repositories
@@ -7,12 +8,12 @@ namespace Catalog.Repositories
 {
         public interface IItemsRepository
     {
-        Item GetItem(Guid id);
-        IEnumerable<Item> GetItems();
+        Task <Item> GetItemAsync(Guid id);
+        Task <IEnumerable<Item>> GetItemsAsync();
 
-        void CreateItem(Item item);
+        Task CreateItemAsync(Item item);
 
-        void UpdateItem(Item item);
-        void DeleteItem(Guid id);
+        Task UpdateItemAsync(Item item);
+        Task DeleteItemAsync(Guid id);
     }
 }
--- Repositories/MongoDBItemsRepository.cs
+++ Repositories/MongoDBItemsRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Catalog.Entities;
 using MongoDB.Bson;
 using MongoDB.Driver;
@@ -21,29 +22,29 @@ namespace Catalog.Repositories
             itemsCollection = database.GetCollection<Item>(collectionName);
         }
 
-        public void CreateItem(Item item)
+        public async Task CreateItemAsync(Item item)
         {
-            itemsCollection.InsertOne(item);
+            await itemsCollection.InsertOneAsync(item);
         }
 
-        public void DeleteItem(Guid id)
+        public void DeleteItemAsync(Guid id)
         {
             var filter = filterBuilder.Eq(item => item.Id, id);
             itemsCollection.DeleteOne(filter);
         }
 
-        public Item GetItem(Guid id)
+        public Item GetItemAsync(Guid id)
         {
             var filter = filterBuilder.Eq(item => item.Id, id);
             return itemsCollection.Find(filter).SingleOrDefault();
         }
 
-        public IEnumerable<Item> GetItems()
+        public IEnumerable<Item> GetItemsAsync()
         {
             return itemsCollection.Find(new BsonDocument()).ToList();
         }
 
-        public void UpdateItem(Item item)
+        public void UpdateItemAsync(Item item)
         {
             var filter = filterBuilder.Eq(existingItem => existingItem.Id, item.Id);
             itemsCollection.ReplaceOne(filter, item);
